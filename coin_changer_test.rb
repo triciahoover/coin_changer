3dd43eac65969a457f37784d396177430fef17b5
triciahoover <tricia.l.hoover@gmail.com>
2017-07-17 13:44:48 -0400

    Initial Test

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -1,9 +1,13 @@
 require "minitest/autorun"
 require_relative "coin_changer.rb"
 
-class Test < Minitest::Test
+class TestCoinChanger < Minitest::Test
 
 	def test_assert_that_1_equals_1
 		assert_equal(1,1)
+	end
+
+	def test_assert_that_zero_cents_equal_
+		assert_equal({}, get_change())
 	end
 end
